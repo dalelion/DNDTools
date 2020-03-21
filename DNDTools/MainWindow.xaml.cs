9670ce461ae478df01e566ee772750b3ec83dad3
zengr <zengr@wit.edu>
2020-03-21 15:20:25 -0400

    Added two comments on the two buttons

--- DNDTools/MainWindow.xaml.cs
+++ DNDTools/MainWindow.xaml.cs
@@ -21,5 +21,27 @@ namespace DNDTools {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private int Button_Click(object sender, RoutedEventArgs e)
+        {
+            int diceRollVal = 1;
+
+            /*
+             * Generates a random value (1-n)
+             * where n is the maximum value of the selected die
+             * The value then changes the button text and sends
+             * a message to the chat log
+             */
+
+            return diceRollVal;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            /*  
+             * Make a new menu open up
+             * Allows Player to choose the different types of dice
+             */ 
+        }
     }
 }
