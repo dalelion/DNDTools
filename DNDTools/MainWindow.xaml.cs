7b7261ab4db4ff17c895de761b3a8ed18b831c78
Noah D'Alelio <dalelion@wit.edu>
2020-03-26 00:47:06 -0400

    Committing partially-complete changes on the dice rolling system to pull in new UI changes.

--- DNDTools/MainWindow.xaml.cs
+++ DNDTools/MainWindow.xaml.cs
@@ -67,5 +67,67 @@ namespace DNDTools {
             Chatlog.Content = Chatlog.Content + "\nPlayer 1's turn has ended. Player 2's turn begins.";
             Chatlog.ScrollToEnd();
         }
+
+        /// <summary>
+        /// Detects when user hits enter key to signal inpout of a command or text to the chat. Can be expanded on in the future.
+        /// </summary>
+        /// <param name="sender">Eventually will contain info about who the request is from for username information</param>
+        /// <param name="e">The key that is pressed. We are looking for the enter key, others can be used here</param>
+        private void Chatbox_KeyDown( object sender, KeyEventArgs e ) {
+
+            if (e.Key.Equals(Key.Enter)) { //User submitted request
+
+                if ( Chatbox.Text.StartsWith( "/" ) ) { //Request is a command
+                    ProcessCommand( Chatbox.Text.ToLower().Split() );
+                } else { //Request is a chat message
+
+                }
+
+                //TODO: @Noah - I want to allow users to hit up and be able to reselect old requests they entered during this session. This will require storing this info before clearning.
+                Chatbox.Text = ""; //Clear chatbox after input is dealt with
+
+            }
+
+        }
+
+        private static void ProcessCommand(String[] Command) {
+
+            /*switch ( Command[0] ) {
+
+                case "/help":
+                    //TODO: @Noah Display commands available to users. Future work to implement this and add more commands
+                    break;
+                case "/roll":
+
+                    break;
+                default:
+                    break;
+
+            }*/
+
+            if ( Command[0].Equals("/roll") && Command[1].Contains('d')) { //temporary rigging to make only /roll work since other commands arent implemented.
+                String[] DiceRoll = Command[1].Split( 'd' );
+                StringBuilder SB = new StringBuilder();
+                for (int i = 0; i < Int32.Parse(DiceRoll[0]); i++ ) {
+                    //SB.Append();
+                    //TODO: @Noah Display each dice roll seperated by +'s and the sum in the chat log.
+                }
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Rolls dice with specified Number of Sides
+        /// </summary>
+        /// <param name="NumSides">How many sides you want the dice being rolled to have.</param>
+        /// <returns>Random number between 1 and NumSides</returns>
+        private static int DiceRoll(int NumSides) {
+
+            return new Random().Next( 1, NumSides + 1 );
+
+        }
+
     }
 }
