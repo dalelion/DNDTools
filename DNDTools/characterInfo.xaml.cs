f3b99afbe9bd05c18535c0b1e8643ebda3c05b05
zengr <zengr@wit.edu>
2020-04-02 17:46:30 -0400

    Remade Character Info Page as xaml, fixed calculations

--- DNDTools/characterInfo.xaml.cs
+++ DNDTools/characterInfo.xaml.cs
@@ -20,17 +20,463 @@ namespace DNDTools
     public partial class Window3 : Window
     {
 
-        Window parent;
+        MainWindow parent;
         Player player;
 
-        public Window3(Window myParent, Player myPlayer)
+        public Window3(MainWindow myParent, Player myPlayer)
         {
             InitializeComponent();
 
             parent = myParent;
             player = myPlayer;
 
-            
+            lblStrConst.Content = parent.StrLbl.Content;
+            lblDexConst.Content = parent.DexLbl.Content;
+            lblConConst.Content = parent.ConLbl.Content;
+            lblIntConst.Content = parent.IntLbl.Content;
+            lblWisConst.Content = parent.WisLbl.Content;
+            lblChaConst.Content = parent.ChaLbl.Content;
+
+            lblStrVar.Content = parent.StrVarLbl.Content;
+            lblDexVar.Content = parent.DexVarLbl.Content;
+            lblConVar.Content = parent.ConVarLbl.Content;
+            lblIntVar.Content = parent.IntVarLbl.Content;
+            lblWisVar.Content = parent.WisVarLbl.Content;
+            lblChaVar.Content = parent.ChaVarLbl.Content;
+
+            //Numbers in parenthesis increase/decrease every 2 values, the MAIN values should be controlled by the player
+        }
+
+        private void btStrUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(0) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(0, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(0, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblStrConst.Content = "" + tempBase;
+                    lblStrVar.Content = "(+" + tempVar + ")";
+
+                    parent.StrLbl.Content = "" + tempBase;
+                    parent.StrVarLbl.Content = lblStrVar.Content;
+                }
+                else
+                {
+                    lblStrConst.Content = "" + tempBase;
+                    lblStrVar.Content = "(" + tempVar + ")";
+
+                    parent.StrLbl.Content = "" + tempBase;
+                    parent.StrVarLbl.Content = lblStrVar.Content;
+                }
+            }
+        }
+
+        private void btStrDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(0) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(0, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(0, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblStrConst.Content = "" + tempBase;
+                    lblStrVar.Content = "(+" + tempVar + ")";
+
+                    parent.StrLbl.Content = "" + tempBase;
+                    parent.StrVarLbl.Content = lblStrVar.Content;
+                }
+                else
+                {
+                    lblStrConst.Content = "" + tempBase;
+                    lblStrVar.Content = "(" + tempVar + ")";
+
+                    parent.StrLbl.Content = "" + tempBase;
+                    parent.StrVarLbl.Content = lblStrVar.Content;
+                }
+            }
+        }
+
+        private void btDexUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(1) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(1, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(1, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblDexConst.Content = "" + tempBase;
+                    lblDexVar.Content = "(+" + tempVar + ")";
+
+                    parent.DexLbl.Content = "" + tempBase;
+                    parent.DexVarLbl.Content = lblDexVar.Content;
+                }
+                else
+                {
+                    lblDexConst.Content = "" + tempBase;
+                    lblDexVar.Content = "(" + tempVar + ")";
+
+                    parent.DexLbl.Content = "" + tempBase;
+                    parent.DexVarLbl.Content = lblDexVar.Content;
+                }
+            }
+        }
+
+        private void btDexDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(1) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(1, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(1, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblDexConst.Content = "" + tempBase;
+                    lblDexVar.Content = "(+" + tempVar + ")";
+
+                    parent.DexLbl.Content = "" + tempBase;
+                    parent.DexVarLbl.Content = lblDexVar.Content;
+                }
+                else
+                {
+                    lblDexConst.Content = "" + tempBase;
+                    lblDexVar.Content = "(" + tempVar + ")";
+
+                    parent.DexLbl.Content = "" + tempBase;
+                    parent.DexVarLbl.Content = lblDexVar.Content;
+                }
+            }
+        }
+
+        private void btConUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(2) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(2, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(2, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblConConst.Content = "" + tempBase;
+                    lblConVar.Content = "(+" + tempVar + ")";
+
+                    parent.ConLbl.Content = "" + tempBase;
+                    parent.ConVarLbl.Content = lblConVar.Content;
+                }
+                else
+                {
+                    lblConConst.Content = "" + tempBase;
+                    lblConVar.Content = "(" + tempVar + ")";
+
+                    parent.ConLbl.Content = "" + tempBase;
+                    parent.ConVarLbl.Content = lblConVar.Content;
+                }
+            }
+        }
+
+        private void btConDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(2) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(2, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(2, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblConConst.Content = "" + tempBase;
+                    lblConVar.Content = "(+" + tempVar + ")";
+
+                    parent.ConLbl.Content = "" + tempBase;
+                    parent.ConVarLbl.Content = lblConVar.Content;
+                }
+                else
+                {
+                    lblConConst.Content = "" + tempBase;
+                    lblConVar.Content = "(" + tempVar + ")";
+
+                    parent.ConLbl.Content = "" + tempBase;
+                    parent.ConVarLbl.Content = lblConVar.Content;
+                }
+            }
+        }
+
+        private void btIntUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(3) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(3, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(3, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblIntConst.Content = "" + tempBase;
+                    lblIntVar.Content = "(+" + tempVar + ")";
+
+                    parent.IntLbl.Content = "" + tempBase;
+                    parent.IntVarLbl.Content = lblIntVar.Content;
+                }
+                else
+                {
+                    lblIntConst.Content = "" + tempBase;
+                    lblIntVar.Content = "(" + tempVar + ")";
+
+                    parent.IntLbl.Content = "" + tempBase;
+                    parent.IntVarLbl.Content = lblIntVar.Content;
+                }
+            }
+        }
+
+        private void btIntDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(3) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(3, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(3, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblIntConst.Content = "" + tempBase;
+                    lblIntVar.Content = "(+" + tempVar + ")";
+
+                    parent.IntLbl.Content = "" + tempBase;
+                    parent.IntVarLbl.Content = lblIntVar.Content;
+                }
+                else
+                {
+                    lblIntConst.Content = "" + tempBase;
+                    lblIntVar.Content = "(" + tempVar + ")";
+
+                    parent.IntLbl.Content = "" + tempBase;
+                    parent.IntVarLbl.Content = lblIntVar.Content;
+                }
+            }
+        }
+
+        private void btWisUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(4) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(4, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(4, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblWisConst.Content = "" + tempBase;
+                    lblWisVar.Content = "(+" + tempVar + ")";
+
+                    parent.WisLbl.Content = "" + tempBase;
+                    parent.WisVarLbl.Content = lblWisVar.Content;
+                }
+                else
+                {
+                    lblWisConst.Content = "" + tempBase;
+                    lblWisVar.Content = "(" + tempVar + ")";
+
+                    parent.WisLbl.Content = "" + tempBase;
+                    parent.WisVarLbl.Content = lblWisVar.Content;
+                }
+            }
+        }
+
+        private void btWisDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(4) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(4, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(4, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblWisConst.Content = "" + tempBase;
+                    lblWisVar.Content = "(+" + tempVar + ")";
+
+                    parent.WisLbl.Content = "" + tempBase;
+                    parent.WisVarLbl.Content = lblWisVar.Content;
+                }
+                else
+                {
+                    lblWisConst.Content = "" + tempBase;
+                    lblWisVar.Content = "(" + tempVar + ")";
+
+                    parent.WisLbl.Content = "" + tempBase;
+                    parent.WisVarLbl.Content = lblWisVar.Content;
+                }
+            }
+        }
+
+        private void btChaUp_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(5) + 1;
+
+            if (tempBase <= 20)
+            {
+                player.setBaseStat(5, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(5, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblChaConst.Content = "" + tempBase;
+                    lblChaVar.Content = "(+" + tempVar + ")";
+
+                    parent.ChaLbl.Content = "" + tempBase;
+                    parent.ChaVarLbl.Content = lblChaVar.Content;
+                }
+                else
+                {
+                    lblChaConst.Content = "" + tempBase;
+                    lblChaVar.Content = "(" + tempVar + ")";
+
+                    parent.ChaLbl.Content = "" + tempBase;
+                    parent.ChaVarLbl.Content = lblChaVar.Content;
+                }
+            }
+        }
+
+        private void btChaDown_Click(object sender, RoutedEventArgs e)
+        {
+            int tempBase = player.getBaseStat(5) - 1;
+
+            if (tempBase >= 0)
+            {
+                player.setBaseStat(5, tempBase);
+
+                int tempVar = tempBase - 10;
+
+                if (tempVar < 0)
+                    tempVar--;
+
+                tempVar /= 2;
+
+                player.setVariableStat(5, tempVar);
+
+                if (tempVar > 0)
+                {
+                    lblChaConst.Content = "" + tempBase;
+                    lblChaVar.Content = "(+" + tempVar + ")";
+
+                    parent.ChaLbl.Content = "" + tempBase;
+                    parent.ChaVarLbl.Content = lblChaVar.Content;
+                }
+                else
+                {
+                    lblChaConst.Content = "" + tempBase;
+                    lblChaVar.Content = "(" + tempVar + ")";
+
+                    parent.ChaLbl.Content = "" + tempBase;
+                    parent.ChaVarLbl.Content = lblChaVar.Content;
+                }
+            }
         }
     }
 }
